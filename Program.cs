e25038b0e94b578c1cd99ac659d1fc97a4834513
ivanov.a <sasha19337@mail.ru>
2024-11-20 16:04:45 +0300

    Добавлена переменная a

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Изменения в ветке develop");
 Console.WriteLine("Всем привет!");
+
+var a = "Снова привет!";
+Console.WriteLine(a);
