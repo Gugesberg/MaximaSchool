6b39096e614b0a578486f0a4f38172b61d191be9
ivanov.a <sasha19337@mail.ru>
2024-12-05 13:22:07 +0300

    Палиндром

--- Program.cs
+++ Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Изменения в ветке develop");
-Console.WriteLine("Всем привет!");
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+bool isPalindrom = default;
+string strNumber = number.ToString();
+for (int i = 0; i < strNumber.Length / 2; i++)
+{
+    if (strNumber[i] == strNumber[strNumber.Length - i - 1])
+    {
+        isPalindrom = true;
+    }
+    else
+    {
+        isPalindrom = false;
+    }
+}
+if (isPalindrom == true)
+{
+    Console.WriteLine("Палиндром");
+}
+else
+{
+    Console.WriteLine("Не палиндром");
+}
+    
+
+
