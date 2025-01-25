5b2d1ddd240f46c2721b0a399ce1e965b2f05150
ivanov.a <sasha19337@mail.ru>
2025-01-25 08:50:14 +0300

    MyList переделан на Generic

--- Program.cs
+++ Program.cs
@@ -9,31 +9,33 @@ using MyCustomList2;
 using System.Reflection;
 
 
-/*MyList list = new MyList(10,5);
-list.Resize();
-Console.WriteLine(list[0]);
-Console.WriteLine(list.Capacity);*/
+using MyCustomList2;
+using System.ComponentModel.DataAnnotations;
 
+namespace MaximaSchool
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
 
-/*int[] little = new int[5];
-int[] big = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            List<int> a = new List<int> { 1, 2, 3, 4, 5 };
+            for (int i = 0; i < a.Count; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
 
-MyList.Copy(big, little);*/
+            var enuemerator = a.GetEnumerator();
+            while (enuemerator.MoveNext())
+            {
+                Console.WriteLine(enuemerator);
+            }
 
-
-MyList list = new MyList();
-list.Add(1); // индекс 0
-list.Add(5); // индекс 1
-list.Add(6); // индекс 2
-
-int[] arr = new int[] { 2, 3, 4 };
-
-list.Add(3,arr);
-
-
-Console.WriteLine(list[0]);
-Console.WriteLine(list[1]);
-Console.WriteLine(list[2]);
-Console.WriteLine(list[3]);
-Console.WriteLine(list[4]);
-Console.WriteLine(list[5]);
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
